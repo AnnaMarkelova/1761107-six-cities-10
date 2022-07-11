de5ace38cae9f4a60f482555ce010201a92a70bf
Anna Markelova <anytkamarkelova@yandex.ru>
2022-07-11 20:34:49 +0200

    adds components: main screen and place-card

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -6,8 +6,16 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
+const Setting = {
+  CARDS_NUMBER: 5,
+  FOUND_PLACES: 300,
+};
+
 root.render(
   <React.StrictMode>
-    <App />
+    <App
+      cardNumber={Setting.CARDS_NUMBER}
+      foundPlaces={Setting.FOUND_PLACES}
+    />
   </React.StrictMode>,
 );
